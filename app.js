8167129709e4823002c98da4508ea6e9a2a1eda0
TechYGG-JAVA <yagogoulartguimaraes@gmail.com>
2026-03-11 10:09:03 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,89 @@
+// let par = 31;
+
+// if (par % 2 == 0){
+//     console.log("o numero e par", par)
+// }else{
+//     console.log("o numero é impar", par)
+// }
+
+// let nome = "Julia";
+// let idade = (2026 - 2012);
+// let nota1 = 12;
+// let nota2 = 3;
+// let nota3 = 12;
+// let resultado = (nota1+nota2+nota3);
+// let media = (resultado/3);
+
+
+
+// if(idade <=17){
+//     console.log("A(o) aluna(o) ", nome, " é menor de idade")
+// }else{
+//     console.log("O(a) aluno(a) ", nome, " é maior de idade")
+// }
+
+// let i = 0;
+
+// while (i<= 20){
+
+//     if (i% 3 == 0){
+//         console.log("Acrécimo de 100%", (i+ (i * 1)));
+//     }
+//     else{
+//         console.log("nao tem acrécimo otaro", i);
+//     }
+//     i++;
+// }
+
+// let i = -50;
+
+// while (i <= 50) {
+//     if (i == 0) {
+//         console.log(i, "O numero é nulo");
+//     }
+//     if (i >= 1) {
+//         console.log(i, "Os numeros positivos tem acrecimo de 75%", (i + (i * 0.75)));
+//     }
+//     else if (i < 0) {
+//         console.log(i, "O numero é descontado de 125%", (i - (i * 1.25)));
+//     }
+
+//     i++
+// }
+
+
+// IF
+// 1. Crie um programa que receba um número em uma variável e verifique se ele é positivo, negativo ou zero, mostrando o resultado no console.
+// 2. Crie um programa que receba uma idade e informe se a pessoa é maior de idade ou menor de idade.
+// 3. Crie um programa que receba um número e verifique se ele é par ou ímpar.
+// 4. Crie um programa que receba dois números e mostre qual deles é o maior.
+// 5. Crie um programa que simule um login simples. Verifique se o usuário e a senha digitados correspondem a valores previamente definidos no código.
+
+//1.
+
+// let numero = 0;
+
+// if(numero == 0){
+
+//     console.log("este numero é nulo: ", numero);
+// }
+// else if(numero <=0){
+
+//     console.log("este numero é negativo: ", numero);
+// }
+// else{
+
+//     console.log("este numero é positivo: ", numero);
+// }
+
+// 2.
+
+let idade = 16;
+
+if(idade <=17){
+    console.log("Voce é menor de idade: ", idade);
+}
+
+else {
+    console.log("Voce é maior de idade");
+}
---
+++ exec.js
@@ -0,0 +1,36 @@
+
+// IF
+// 1. Crie um programa que receba um número em uma variável e verifique se ele é positivo, negativo ou zero, mostrando o resultado no console.✔
+// 2. Crie um programa que receba uma idade e informe se a pessoa é maior de idade ou menor de idade.✔
+// 3. Crie um programa que receba um número e verifique se ele é par ou ímpar.✔
+// 4. Crie um programa que receba dois números e mostre qual deles é o maior.✔
+// 5. Crie um programa que simule um login simples. Verifique se o usuário e a senha digitados correspondem a valores previamente definidos no código.✔
+
+//1.
+
+// let numero = 0;
+
+// if(numero == 0){
+
+//     console.log("este numero é nulo: ", numero);
+// }
+// else if(numero <=0){
+
+//     console.log("este numero é negativo: ", numero);
+// }
+// else{
+
+//     console.log("este numero é positivo: ", numero);
+// }
+
+//2.
+
+let idade = 17;
+
+if(idade < 18){
+    console.log("Voce é menor de idade: ", idade);
+}
+
+else {
+    console.log("Voce é maior de idade", idade);
+}
